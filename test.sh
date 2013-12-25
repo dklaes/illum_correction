9dd047188b8187ca9d414e6d0074cf15cd34f13a
Dominik Klaes <dklaes@astro.uni-bonn.de>
2013-12-26 00:02:18 +0100

    "test.sh": New modifications.

--- test.sh
+++ test.sh
@@ -1,8 +1,15 @@
+#!/bin/bash -xv
+
 SIGMA=0.0503426
 MAIND=/home/dklaes/git/illum_correction/testing/run_11_08_f/
 STANDARDD=STANDARD_r_SDSS
 NIGHT=0
+NPARA=1
 
 . ./OMEGACAM.ini
 
-./illum_correction_contourplot_fitfunction.py ${MAIND}/${STANDARDD}/calib/residuals_${NIGHT}/ -${SIGMA} ${SIGMA}
+rm testing/run_11_08_f/STANDARD_r_SDSS/calib/residuals_0/*.fits
+rm testing/run_11_08_f/STANDARD_r_SDSS/calib/residuals_0/*.png
+rm testing/run_11_08_f/STANDARD_r_SDSS/calib/residuals_0/single_plots/*.png
+
+python illum_correction_contourplot_fitfunction.py ${MAIND}/${STANDARDD}/calib/residuals_${NIGHT}/ -${SIGMA} ${SIGMA}
