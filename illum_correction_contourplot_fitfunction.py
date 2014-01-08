c7564b281c474fd44228e466623ed22c7a4df3cd
Dominik Klaes <dklaes@astro.uni-bonn.de>
2014-01-08 16:41:01 +0100

    "illum_correction_contourplot_fitfunction.py": Added pyth to python.

--- illum_correction_contourplot_fitfunction.py
+++ illum_correction_contourplot_fitfunction.py
@@ -1,5 +1,7 @@
+#!/usr/bin/python
 # -*- coding: utf-8 -*-
 #scipy-0.11 and numpy-1.6.2 required!
+
 
 # ----------------------------------------------------------------
 # File Name:           illum_correction_contourplot_fitfunction.py
