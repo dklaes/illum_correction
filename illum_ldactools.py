2d24db1571208a8e7572cf8b5b12cf9727360977
Dominik Klaes <dklaes@astro.uni-bonn.de>
2014-01-17 12:29:45 +0100

    "illum_ldactools.py": Added "FILTER_MAGNITUDE" option to exclude objects above and below given magnitude limits.

--- illum_ldactools.py
+++ illum_ldactools.py
@@ -191,3 +191,29 @@ elif (action == 'FILTER_RESIDUALMEAN'):
   data3 = filter_elements(data2, key, uppercutresmean+mean, '<')
   
   data3.saveas(outfile, clobber=replace)
+elif (action == 'FILTER_RESIDUAL'):
+  # The following argument have to within the "external" string in the following order:
+  # lowercutresabs, uppercutresabs
+  replace=True
+  data = ldac.LDACCat(infile)[table]
+  
+  lowercutresabs = float(external[0])
+  uppercutresabs = float(external[1])
+  
+  data2 = filter_elements(data, key, lowercutresabs, '>')
+  data3 = filter_elements(data2, key, uppercutresabs, '<')
+  
+  data3.saveas(outfile, clobber=replace)
+elif (action == 'FILTER_MAGNITUDE'):
+  # The following argument have to within the "external" string in the following order:
+  # lowercutmag, uppercutmag
+  replace=True
+  data = ldac.LDACCat(infile)[table]
+  
+  lowercutmag = float(external[0])
+  uppercutmag = float(external[1])
+  
+  data2 = filter_elements(data, key, lowercutmag, '>')
+  data3 = filter_elements(data2, key, uppercutmag, '<')
+  
+  data3.saveas(outfile, clobber=replace)
