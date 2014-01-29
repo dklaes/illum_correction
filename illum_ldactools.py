c4fb05ea2fd3b0dec63694d0d3989fd41d925582
Dominik Klaes <dklaes@astro.uni-bonn.de>
2014-01-29 15:27:00 +0100

    "illum_ldactools.py": Added MAG_DEPENDENCY action with corresponding fitting procedures.

--- illum_ldactools.py
+++ illum_ldactools.py
@@ -8,6 +8,7 @@ import os
 import sys
 import getopt
 import ldac
+from scipy.optimize import curve_fit
 
 
 def filter_elements(data, key, value, condition):
@@ -425,9 +426,94 @@ def statistics(infile, outfile, table, external, coordinates):
     else:
       f.write("%s %.5f %.5f\n" % (name, output[name + '_before'], output[name + '_after']))
   f.close()
+
+
+
+# Magnitude - residual dependency
+
+def function_linear_plusOffset(x, A, B):
+	return A*x + B
+
+def function_linear(x, A):
+	return A*x
+
+def mag_dependency(infile, outfile, table, realisation=0):
+  # Importing catalogues.
+  data = ldac.LDACCat(infile)[table]
+  fmagdependency = open(outfile, "w")
   
-  
-  
+  for k in range(NUMCHIPS+1):
+	if ( k == 0 ):
+	  data2 = data
+	else:
+	  data2 = filter_elements(data, 'CHIP',k , '=')
+
+	MagZP = np.array(data2['MagZP'], dtype=np.float64)
+	Residual = np.array(data2['Residual'], dtype=np.float64)
+	Residual_Err = np.array(data2['Residual_Err'], dtype=np.float64)
+	Mag_fitted = np.array(data2['Mag_fitted'], dtype=np.float64)
+	Residual_fitted = np.array(data2['Residual_fitted'], dtype=np.float64)
+	Residual_fitted_Err = np.array(data2['Residual_fitted_Err'], dtype=np.float64)
+	
+	# A = coefficient A
+	# B = coefficient B
+	# w = with
+	# wo = without
+	# O = Offset
+	# b = before fitting
+	# a = after fitting
+	# F = Fitting
+	# Linear fit with offset before illumination correction
+	p0 = np.array([0.0, 0.0])
+	best_par, cov_fit = curve_fit(function_linear_plusOffset, MagZP, Residual, p0, sigma=Residual_Err)
+	error = np.sqrt(np.diagonal(cov_fit))
+	AwObF = best_par[0]
+	BwObF = best_par[1]
+	AwObF_Err = error[0]
+	BwObF_Err = error[1]
+
+	# Linear fit with offset after illumination correction
+	p0 = np.array([0.0, 0.0])
+	best_par, cov_fit = curve_fit(function_linear_plusOffset, Mag_fitted, Residual_fitted, p0, sigma=Residual_fitted_Err)
+	error = np.sqrt(np.diagonal(cov_fit))
+	AwOaF = best_par[0]
+	BwOaF = best_par[1]
+	AwOaF_Err = error[0]
+	BwOaF_Err = error[1]
+
+	# Linear fit without offset before illumination correction
+	p0 = np.array([0.0])
+	best_par, cov_fit = curve_fit(function_linear, MagZP, Residual, p0, sigma=Residual_Err)
+	error = np.sqrt(np.diagonal(cov_fit))
+	AwoObF = best_par[0]
+	AwoObF_Err = error[0]
+
+	# Linear fit without offset after illumination correction
+	p0 = np.array([0.0])
+	best_par, cov_fit = curve_fit(function_linear, Mag_fitted, Residual_fitted, p0, sigma=Residual_fitted_Err)
+	error = np.sqrt(np.diagonal(cov_fit))
+	AwoOaF = best_par[0]
+	AwoOaF_Err = error[0]
+		
+	# Writing to file
+	# Format:
+	# 1: realisation (only important for bootstrapping, otherwise per default 0)
+	# 2: chip (if 0 then all chips were taken into account)
+	# 3: A with offset, before fitting
+	# 4: A_Err with offset, before fitting
+	# 5: B with offset, before fitting
+	# 6: B_Err with offset, before fitting
+	# 7: A with offset, after fitting
+	# 8: A_Err with offset, after fitting
+	# 9: B with offset, after fitting
+	# 10: B_Err with offset, after fitting
+	# 11: A without offset, before fitting
+	# 12: A_Err without offset, before fitting
+	# 13: A without offset, after fitting
+	# 14: A_Err without offset, after fitting
+	fmagdependency.write("%i %i %f %f %f %f %f %f %f %f %f %f %f %f\n" %(realisation, k, AwObF, AwObF_Err, BwObF, BwObF_Err, AwOaF, AwOaF_Err, BwOaF, BwOaF_Err, AwoObF, AwoObF_Err, AwoOaF, AwoOaF_Err))
+
+  fmagdependency.close()
 
 
 opts, args = getopt.getopt(sys.argv[1:], "i:o:t:k:a:v:c:e:", ["input=", "output=", "table=", "key=", "action=", "value=", "condition=", "external="])
@@ -658,3 +744,6 @@ elif (action == 'CHECK_ENOUGH_OBJECTS'):
   data = ldac.LDACCat(infile[0])[table]
   data_chip = filter_elements(data, 'CHIP', value, "=")
   print(len(data_chip))
+
+elif (action == 'MAG_DEPENDENCY'):
+  mag_dependency(infile[0], outfile, table)
